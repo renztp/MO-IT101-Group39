edd90e303b009e02e7d9e7147e934d3f312b88b8
renztp <tullaorenz@gmail.com>
2026-03-23 21:31:09 +0800

    added new functions to make processPayrollStaff more smaller and manageable, added some logic code from kristine bordo when handling totalHoursWorked decimal calculation and payroll flow

--- src/test/java/MOIT101Group39Test.java
+++ src/test/java/MOIT101Group39Test.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,7 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MOIT101Group39Test {
 
     @Test
+    @DisplayName("Should calculate hours correctly based on timeIn and timeOut")
     void calculateTotalHoursWorked() {
         assertEquals(8.0, MOIT101Group39.calculateTotalHoursWorked(8.0, 17.0));
+    }
+
+    @Test
+    @DisplayName("Should parse string to correct decimal")
+    void shouldParseStringTimeToDecimal() {
+        assertEquals(8.0, MOIT101Group39.parseTimeToDecimal("8:0"));
     }
 }
