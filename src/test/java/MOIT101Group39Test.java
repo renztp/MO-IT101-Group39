6a2ee399027207e734217d05820070bec0ae17cf
renztp <tullaorenz@gmail.com>
2026-03-23 11:50:09 +0800

    added an initial test

---
+++ src/test/java/MOIT101Group39Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MOIT101Group39Test {
+
+    @Test
+    void calculateTotalHoursWorked() {
+        assertEquals(8.0, MOIT101Group39.calculateTotalHoursWorked(8.0, 17.0));
+    }
+}
